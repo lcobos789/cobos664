0fb9f8a8d1b44bcd9a57f8a0c6823d3043c369fc
Laura Cobos <Laura@jonathans-mbp-2.myfiosgateway.com>
2023-10-16 20:35:27 -0400

    week4

---
+++ week_4/week_4.py
@@ -0,0 +1,28 @@
+import csv
+import os
+
+with open('week_4/Artworks.csv', 'r', newline='') as csv_file:
+    artworks_csv_file = csv.DictReader(csv_file)
+
+    os.makedirs('week_4/res', exist_ok=True)
+
+nat_files = {}
+
+with open ('Artworks.csv') as artworks_file:
+    artworks_csv_file = csv.DictReader(artworks_file)
+    for artwork in artworks_csv_file:
+        nationalities_str = artwork['Nationality']
+        nationalities = nationalities_str.split(' ')
+    for nat in nationalities:
+   
+      if nat_files.get(nat) is None:
+        with open("art_nationalities_files/{nat}.csv", "w") as nat_file:
+
+      nat_dict_writer = csv.DictWriter(nat_file, artworks_csv_file.fieldnames)
+      nat_dict_writer .writeheader()
+      nat_dict_writer .writerow(artwork)
+      nat_files[nat] = True
+    else:
+       with open(f"art_nationalities_files/{nat}.csv", "a") as nat_file:
+          nat_dict_writer = csv.DictWriter(nat_file, artworks_csv_file.fieldnames)
+          nat_dict_writer.writerow(artwork)
